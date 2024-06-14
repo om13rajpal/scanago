5b233789752c6e1f581a7c8d61297d8b2c26e19d
Om Rajpal <omrajpal.exe@gmail.com>
2024-06-14 13:43:46 +0530

    introducted a basic backend express server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("this server is running properly")
+})
+
+app.listen(port, ()=>{
+    console.log(`your server is running on http://localhost:${port}`);
+    
+})
