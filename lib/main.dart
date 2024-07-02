f1be74a4956727f003beb870373883a9a2bd8603
Om Rajpal <omrajpal.exe@gmail.com>
2024-07-02 22:22:27 +0530

    added the functionality to redirect the user to dashboard if he has already logged in before

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:scanago/dashboard.dart';
 import 'package:scanago/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const Scanago());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String? token = prefs.getString('token');
+  runApp(Scanago(
+    token: token,
+  ));
 }
 
-class Scanago extends StatelessWidget {
-  const Scanago({super.key});
+class Scanago extends StatefulWidget {
+  final dynamic token;
+  const Scanago({super.key, this.token});
+
+  @override
+  State<Scanago> createState() => _ScanagoState();
+}
+
+class _ScanagoState extends State<Scanago> {
+  late bool loggedIn = false;
+
+  @override
+  void initState() {
+    checkToken();
+    super.initState();
+  }
+
+  void checkToken() {
+    if (widget.token != null) {
+      if (!JwtDecoder.isExpired(widget.token)) {
+        loggedIn = true;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +45,7 @@ class Scanago extends StatelessWidget {
       theme: ThemeData(
           primaryColor: const Color(0xFFF8F4EA),
           visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: const Login(),
+      home: (loggedIn) ? Dashboard(token: widget.token) : const Login(),
     );
   }
 }
