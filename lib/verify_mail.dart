90b36a2f11ea81e47e1aeb8006df82bea09c6d38
Om Rajpal <omrajpal.exe@gmail.com>
2024-07-14 17:22:58 +0530

    designed the verify mail page

--- lib/verify_mail.dart
+++ lib/verify_mail.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:lottie/lottie.dart';
 import 'package:scanago/templates/button.dart';
 import 'package:http/http.dart' as http;
+import 'package:scanago/utils/logout.dart';
 
 class VerifyMail extends StatefulWidget {
   final String email;
@@ -41,31 +44,89 @@ class _VerifyMailState extends State<VerifyMail> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(20),
-        color: const Color(0xFFF8F4EA),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xfff3f3f3), Color(0xffd8d8d8)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        padding: const EdgeInsets.only(left: 25, right: 25),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const Text(
-                'your email is not verified Please verify your mail',
-                style: TextStyle(
-                    fontFamily: 'monkey',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500),
-                textAlign: TextAlign.center,
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Positioned(
+                      top: 50,
+                      right: 20,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              logout(context);
+                            },
+                            child: SvgPicture.asset(
+                              width: MediaQuery.of(context).size.width * 0.05,
+                              'assets/images/logout-light.svg',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 100,
+                        child: Text(
+                      'Scanago',
+                      style: TextStyle(
+                          fontFamily: 'inter',
+                          fontWeight: FontWeight.w700,
+                          fontSize: MediaQuery.of(context).size.width * 0.08,
+                          color: const Color(0xff202020)),
+                    )),
+                    LottieBuilder.asset('assets/lottie/man.json'),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Your email is not verified Please verify your mail',
+                          style: TextStyle(
+                              fontFamily: 'inter',
+                              color: const Color(0xff202020),
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.05,
+                              fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Button(
+                            text: 'Resend Verification Mail',
+                            onPressed: () {
+                              sendVerificationMail();
+                            },
+                            radius: 12,
+                            fontSize: MediaQuery.of(context).size.width * 0.032)
+                      ],
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 15,
+                        child: SvgPicture.asset(
+                          'assets/images/arrow.svg',
+                          width: MediaQuery.of(context).size.width * 0.6,
+                        ))
+                  ],
+                ),
               ),
-              const SizedBox(
-                height: 5,
-              ),
-              Button(
-                  text: 'Resend Verification Mail',
-                  onPressed: () {
-                    sendVerificationMail();
-                  },
-                  radius: 12,
-                  fontSize: 12)
             ],
           ),
         ),
